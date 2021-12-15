d7117d2e6b99517f662165174cc0986d3ad3d5af
Csangos <csangos@gmail.com>
2021-12-15 14:33:18 -0500

    initial solution

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+fn load_from_file_commands(file_path: &str) -> Vec<(String, i32)> {
+    let file = File::open(file_path).expect("file wasn't found.");
+    let reader = BufReader::new(file);
+
+    let commands: Vec<String> = reader
+        .lines()
+        .map(|line| line.unwrap().parse::<String>().unwrap())
+        .collect();
+
+    // println!("{:?}", commands[0]);
+
+    let moves: Vec<(String, i32)> = commands
+        .iter()
+        .map(|command| {
+            let mut parts = command.split_whitespace();
+            let command = parts.next().unwrap();
+            let value = parts.next().unwrap().parse::<i32>().unwrap();
+            (command.to_string(), value)
+        })
+        .collect();
+
+    moves
+}
+
+fn dive(commands: Vec<(String, i32)>) {
+    let mut depth = 0;
+    let mut horz = 0;
+    let mut aim = 0;
+    for command in &commands {
+        if command.0 == "forward" {
+            horz += command.1;
+            depth += aim * command.1;
+        } else if command.0 == "down" {
+            aim += command.1;
+        } else if command.0 == "up" {
+            aim -= command.1;
+        }
+        println!("{:?}", (horz, depth, aim));
+    }
+    println!("Horz: {} Depth {} // Answer: {}", horz, depth, horz * depth);
+}
+
+fn main() {
+    let commands = load_from_file_commands("src/course.txt");
+    dive(commands);
+}
